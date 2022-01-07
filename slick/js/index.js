e782d4f6220a5653c8307294ff32ed057a398138
Yanochka <zavgorodnyaya-yana@mail.ru>
2022-01-07 04:58:27 +0300

    Прописала стили для slick-slide

--- slick/js/index.js
+++ slick/js/index.js
@@ -1,26 +1,69 @@
-$(document).ready(function(){
-  $('.menu-slick-slide1').slick({
+$('.menu-slick-slide1').slick({
   slidesToShow: 1,
   dots: true,
   arrows: false,
-});
+  vertical: true,
+  verticalSwiping: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        vertical: false,
+        verticalSwiping: false
+      }
+    }]
 });
 
-$(document).ready(function(){
 $('.comment-slick-slide2').slick({
   slidesToShow: 1,
   arrows: true,
   dots: true,
-});
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        arrows: false
+      }
+    }]
 });
 
-$(document).ready(function(){
 $('.chance-bottom').slick({
   dots: false,
+  infinite:true,
   arrows: false,
   slidesToShow: 5,
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 2000,
-});
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        dots: true,
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 375,
+      settings: {
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }]
 });
